0853a28656cde32a7bbd7fc8eef0db561e1f40da
Lucio Bassani <luciobassani12@gmail.com>
2019-10-22 19:51:08 -0300

    Added mapAffinitiy pretty printer. Added equity calculation. Added function to get affinity between two developers.

--- app/Main.hs
+++ app/Main.hs
@@ -12,9 +12,12 @@ import Github
 main :: IO ()
 main = do
     let
-        credentials = setCredentials "lbassani12" "e9bf8f472a257568c3932fe052980e971dae6db5"
+        credentials = setCredentials <Your User> <Your Password>
         repo = setRepo "Microsoft" "TypeScript"
         date_range = setDate (fromGregorian 2019 10 01) (fromGregorian 2019 10 21)
     pr1 <- getRepoInteractions repo date_range credentials
     let maps = generateTeamAffMap pr1
-    print (length pr1, pr1, maps)
+        eq = getTeamEquity maps
+        pair_aff = getPairAff maps "sandersn" "rbuckton"
+    teamAffMapPP maps
+    print (eq, pair_aff)
--- src/TeamAffinity.hs
+++ src/TeamAffinity.hs
@@ -1,11 +1,12 @@
 module TeamAffinity where
 
 -- LTS Imports
-import qualified Data.HashMap as Map
+import qualified Data.Map as Map
+import qualified Data.Map.Internal.Debug as DMap
+import Numeric.Extra
 
 -- User Imports
 import Types
-
 
 innerMap :: [Developer] -> DevAff
 innerMap = Map.fromListWith (\new_aff old_aff -> new_aff + old_aff) . Prelude.map (\dev -> (dev, 1))
@@ -13,3 +14,18 @@ innerMap = Map.fromListWith (\new_aff old_aff -> new_aff + old_aff) . Prelude.ma
 generateTeamAffMap :: RepoInteractions -> TeamAff
 generateTeamAffMap = Map.fromListWith (\new_map old_map -> Map.unionWith (\aff1 aff2 -> aff1 + aff2) new_map old_map) .
         Prelude.map (\pr -> (author pr, innerMap (reviewers pr)))
+
+teamAffMapPP :: TeamAff -> IO ()
+teamAffMapPP t = putStrLn $ DMap.showTreeWith (\k x -> show (k, Map.toAscList x)) False True t
+
+getPairAff :: TeamAff -> Developer -> Developer -> Int
+getPairAff map d1 d2 = (map Map.! d1) Map.! d2
+
+stdDev :: [Float] -> Float
+stdDev xs = sqrt . average . map ((^2) . (-) axs) $ xs
+            where average = (/) <$> sum <*> realToFrac . length
+                  axs = average xs
+
+getTeamEquity :: TeamAff -> Float
+getTeamEquity t = let values = Prelude.map intToFloat (concat (Prelude.map Map.elems (Map.elems t))) in
+    stdDev values
